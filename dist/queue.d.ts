3a8bb029b6a8cf44a20fcde21a013b6672667c01
Taylor Vann <brian.t.vann@gmail.com>
2025-12-30 22:19:07 -0800

    dist deleted

--- dist/queue.d.ts
+++
@@ -1,6 +0,0 @@
-import type { DispatchParams } from "./type_flyweight.js";
-export interface QueableAtom {
-    dispatchQueueEvent(): void;
-    fetch(): Promise<void> | undefined;
-}
-export declare function queued(dispatchParams: DispatchParams, atom: QueableAtom): boolean;
